1773094da803670c4b327b62bcf33d8fa1da8a28
AlisonFernando2 <alisonteobaldo2001@gmail.com>
2021-03-02 23:17:43 +0000

    Corrigindo Atividade 01

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Olá meu nome é Alison Fernando");
-    System.out.println("Moro em Viradouro SP");
+    System.out.println("Olá meu nome é Alison Fernando e moro em Viradouro SP");
+    
   }
 }
